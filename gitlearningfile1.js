5424ffcf09c587edf8914f88ecd76a1c3d34f50c
wing168 <48693352+wing168@users.noreply.github.com>
2021-02-13 14:36:10 +0000

    Update gitlearningfile1.js

--- gitlearningfile1.js
+++ gitlearningfile1.js
@@ -2,5 +2,5 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 let index = 5;
 const isLoading = false;
 const whatIf = 'this is a string';
-const newField = 'new field';
+const newField = 'new field 1';
 const newField2 = 'new field 2';
