02c3127b9dbce8fafe2294b0bf3d74e208b581a3
wing168 <wh.man@btinternet.com>
2021-02-13 14:22:02 +0000

    update isLoading field

--- gitlearningfile1.js
+++ gitlearningfile1.js
@@ -1,6 +1,6 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 let index = 5;
-const isLoading = false;
+const isLoading = true;
 const whatIf = 'this is a string';
 const newField = 'new field';
 const newField2 = 'new field 2';
