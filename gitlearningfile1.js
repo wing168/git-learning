3496e779e8f68ff79bee98fc55f70d100011af63
wing168 <wh.man@btinternet.com>
2021-02-13 14:37:05 +0000

    update new field variable with updated text

--- gitlearningfile1.js
+++ gitlearningfile1.js
@@ -2,6 +2,6 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 8];
 let index = 5;
 const isLoading = true;
 const whatIf = 'this is a string';
-const newField = 'new field';
+const newField = 'new field a';
 const newField2 = 'new field 2';
 const newField3 = 'new field 3';
