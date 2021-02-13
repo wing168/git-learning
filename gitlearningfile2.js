e0efc6dd6b082ec40faf3555a0ebac768faa510f
wing168 <wh.man@btinternet.com>
2021-02-13 10:42:45 +0000

    first commit

---
+++ gitlearningfile2.js
@@ -0,0 +1,17 @@
+const strArr = ['a', 'b', 'c', 'd', 'e'];
+let option = 'open';
+
+const bubbleSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            let temp = arr[j];
+
+            if(arr[j] > arr[j+1]) {
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+
+    return arr;
+}
